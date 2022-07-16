a8c38be335ec904f23ceb01e8767fa062fb05e88
thamEnJie <tham_en_jie@s2021.ssts.edu.sg>
2022-07-16 10:55:18 +0800

    Initial Commit

---
+++ GetCatApp/GetCatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GetCatAppApp.swift
+//  GetCatApp
+//
+//  Created by Tham En Jie on 16/7/22.
+//
+
+import SwiftUI
+
+@main
+struct GetCatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
